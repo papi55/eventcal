8ef12d1d60f657a83791bce97000007d22d6a214
Laura Stewart <laura.stewart33@gmail.com>
2010-01-24 14:23:15 -0500

    Added a Test Class for Event.java

---
+++ test/eventcal/domain/EventTest.java
@@ -0,0 +1,63 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package eventcal.domain;
+import junit.framework.TestCase;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.text.ParseException;
+import java.util.Date;
+
+/**
+ *
+ * @author lstewart
+ */
+public class EventTest extends TestCase {
+    private Event event;
+    protected void setUp() throws Exception {
+        event = new Event();
+    }
+
+    public void testSetAndGetDescription(){
+        String testDesc = "This is awesome";
+        assertNull(event.getDescription());
+        event.setDescription(testDesc);
+        assertEquals(testDesc, event.getDescription());
+    }
+
+    public void testSetAndGetClub(){
+        String clubname = "Pirate Club";
+        assertNull(event.getClub());
+        event.setClub(clubname);
+        assertEquals(clubname, event.getClub());
+    }
+
+    public void testSetAndGetFinalizedDate(){
+
+        DateFormat df = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            Date today = df.parse("20/12/2005");
+            assertNull(event.getFinalizedDate());
+            event.setFinalisedDate(df.format(today));
+            assertEquals(df.format(today),event.getFinalizedDate());
+        }
+        catch(ParseException e){
+            e.printStackTrace();
+        }
+    }
+
+    public void testGetLastModified(){
+
+            assertNull(event.getLastModified());
+
+    }
+
+    public void testGetDateCreated(){
+
+            assertNull(event.getDateCreated());
+
+    }
+
+}
